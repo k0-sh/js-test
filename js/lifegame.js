9b677b0a2fe60becfddf164fc3c7d084bb338851
k0-sh <plyqly@gmail.com>
2023-12-28 18:54:01 +0900

    lifegame.html 実装

--- js/lifegame.js
+++ js/lifegame.js
@@ -6,8 +6,47 @@ let cols;
 let rows;
 let cell_int;
 let cells = [];
+let timer;
+let buttonStart;
+let buttonRandom;
+let buttonReset;
+let running = false;
+
+window.onload = function() {
+    canvas = document.getElementById('cv');
+    ctx = canvas.getContext('2d');
+    canvas.style.border = "2px solid";
+    canvas.style.borderColor = 'rgb(60, 60, 60)';
+    ctx.fillStyle = 'rgb(255,255,255)';
+    ctx.fillRect(0,0, canvas.width, canvas.height);
+    ctx.lineWidth = 1;
+    cell_int = canvas.width / cellsize;
+    buttonRandom = document.getElementById('buttonRandom');
+    buttonStart = document.getElementById('buttonStart');
+    buttonReset = document.getElementById('buttonReset');
+    buttonRandom.addEventListener('click', random_cells, false);
+    buttonStart.addEventListener('click', onStart, false);
+    buttonReset.addEventListener('click', init_cells, false)
+    draw_first();
+}
 
 
+// 開始
+function onStart() {
+    if (running) {
+        clearInterval(timer);
+        buttonStart.value = "Start";
+        running = false;
+        console.log(running);
+    } else {
+        timer = setInterval(ngenerate, 100);
+        buttonStart.value = "Stop";
+        running = true;
+        console.log(running)
+    }
+}
+
+// 描画
 function draw() {
     ctx.clearRect(0, 0, 320, 320);
     for (let i = 0; i < cell_int; i++) {
@@ -15,13 +54,13 @@ function draw() {
             if (cells[i][j] == 1) {
                 ctx.fillStyle = 'rgb(156, 255,0)'
                 ctx.fillRect(i * cellsize, j * cellsize, cellsize, cellsize);
-                //ctx.strokeStyle = 'black';
-                //ctx.strokeRect(i * cellsize, j * cellsize, cellsize, cellsize);
+                ctx.strokeStyle = 'black';
+                ctx.strokeRect(i * cellsize, j * cellsize, cellsize, cellsize);
             } else {
                 ctx.fillStyle = 'white'
                 ctx.fillRect(i * cellsize, j * cellsize, cellsize, cellsize);
-                //ctx.strokeStyle = 'black';
-                //ctx.strokeRect(i * cellsize, j * cellsize, cellsize, cellsize);
+                ctx.strokeStyle = 'black';
+                ctx.strokeRect(i * cellsize, j * cellsize, cellsize, cellsize);
             }
         }
     }
@@ -57,6 +96,28 @@ function draw_first() {
         }
     }
 } 
+
+
+// ランダムに生成
+
+function random_cells() {
+    for (let i = 0; i < cell_int; i++) {
+        for (let j = 0; j < cell_int; j++) {
+            cells[i][j] = Math.round(Math.random());
+        }
+    }
+    draw();
+}
+
+// 描画をリセットにして停止
+function init_cells() {
+    for (let i = 0; i < cell_int; i++) {
+        for (let j = 0; j < cell_int; j++) {
+            cells[i][j] = 0;
+        }
+    }
+    draw();
+}
 
 
 // 隣接セルの判定
@@ -106,21 +167,12 @@ function ngenerate() {
 function check() {
     //console.log(neighbor(15,15))
     //console.log(cell_int)
-    setInterval(ngenerate, 100)
+    
+    timer = setInterval(ngenerate, 100);
 }
 
 
 
-window.onload = function() {
-    canvas = document.getElementById('cv');
-    ctx = canvas.getContext('2d');
-    canvas.style.border = "2px solid";
-    canvas.style.borderColor = 'rgb(60, 60, 60)';
-    ctx.fillStyle = 'rgb(255,255,255)';
-    ctx.fillRect(0,0, canvas.width, canvas.height);
-    ctx.lineWidth = 1;
-    cell_int = canvas.width / cellsize;
-    draw_first();
-}
+
 
 
